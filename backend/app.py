bef030961b1ff812d4944c48596b7cb438d14110
andrew-lin-chang <andrewchang@utexas.edu>
2025-03-04 00:40:23 -0600

    test mongodb connection

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 from pymongo import MongoClient
+from flask_bcrypt import Bcrypt
 
 load_dotenv(dotenv_path="../.env")
 
 mongo_uri = os.getenv("MONGO_URI")
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 @app.route("/")
 def home():
     try: 
         client = MongoClient(mongo_uri)
-        db = client["sample_mflix"]
-        res = db.list_collection_names()
-        return f"Collections in database: {res}", 200
+        client.admin.command("ping")
+        return jsonify({"message": "Connected to MongoDB"}), 200
 
     except Exception as e:
-        return f"Error connecting: {e}", 500
+        return jsonify({"message": f"Failed to connect to MongoDB", "error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
