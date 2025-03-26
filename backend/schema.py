a26aaf54c6f1db8847504ce0804bd582f28c3d8c
andrew-lin-chang <andrewchang@utexas.edu>
2025-03-26 17:02:37 -0500

    added create project endpoint

--- backend/schema.py
+++ backend/schema.py
@@ -5,17 +5,18 @@ class User(Document):
     password = StringField(required=True)
 
 class Project(Document):
-    name = StringField(required=True, unique=True)
+    project_id = IntField(required=True, unique=True)
+    project_name = StringField(required=True)
     description = StringField()
-    authorized_users = ListField(ReferenceField('User'))
-    hardware_list = ListField(ReferenceField('Hardware'))
+    authorized_users = ListField(ReferenceField('User'), default=list)
+    hardware_list = ListField(ReferenceField('Hardware'), default=list)
 
 class Hardware(Document):
     name = StringField(required=True)
     project = ReferenceField('Project', required=True)
     available = IntField(required=True)
     checked_out = IntField(required=True)
-    checkouts = ListField(ReferenceField('Checkout'))
+    checkouts = ListField(ReferenceField('Checkout'), default=list)
 
 class Checkout(Document):
     user = ReferenceField('User', required=True)
