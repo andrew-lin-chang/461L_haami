bf50145fe99fc3b2e4d96f1b63ec5da7674cb70b
andrew-lin-chang <andrewchang@utexas.edu>
2025-04-10 15:44:22 -0500

    set user state upon automatic login

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,9 +1,11 @@
 import { Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [loading, setLoading] = useState(true);
+  const { setUser } = useAuth();
 
   // check if authToken exists to automatically login user
   useEffect(() => {
@@ -11,6 +13,7 @@ const ProtectedRoute = ({ children }) => {
     const userid = localStorage.getItem("userid");
 
     if (token && userid) {
+      setUser({ userid, token });
       setIsLoggedIn(true);
     }
     setLoading(false);
