e266158e541e7e429ede87d97508af5570c71db8
andrew-lin-chang <andrewchang@utexas.edu>
2025-04-10 17:25:26 -0500

    implemented checkin and checkout

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useAuth } from "./AuthContext";
 import {
   Card,
   CardContent,
@@ -14,89 +15,129 @@ import {
   Button,
 } from "@mui/material";
 
+function HardwareRow({ hardware }) {
+  const [available, setAvailable] = useState(parseInt(hardware.available));
+  const [checkedOut, setCheckedOut] = useState(parseInt(hardware.checked_out));
+  const [quantity, setQuantity] = useState(0);
+
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const { user } = useAuth();
+
+  const handleCheckin = async () => {
+    const response = await fetch(`${apiUrl}/hardware/checkin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userid: user.userid,
+        item: hardware.item,
+        quantity: quantity,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setAvailable(available + parseInt(quantity));
+      setCheckedOut(checkedOut - parseInt(quantity));
+      alert(data.message);
+    } else {
+      alert(data.message);
+      console.error(data);
+    }
+  };
+
+  const handleCheckout = async () => {
+    const response = await fetch(`${apiUrl}/hardware/checkout`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userid: user.userid,
+        item: hardware.item,
+        quantity: quantity,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setAvailable(available - parseInt(quantity));
+      setCheckedOut(checkedOut + parseInt(quantity));
+      alert(data.message);
+    } else {
+      alert(data.message);
+      console.error(data);
+    }
+  };
+
+  return (
+    <TableRow>
+      <TableCell>{hardware.item}</TableCell>
+      <TableCell>{available}</TableCell>
+      <TableCell>{checkedOut}</TableCell>
+      <TableCell>
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+          style={{ width: "50px" }}
+          min="0"
+          max={hardware.available}
+        />
+      </TableCell>
+      <TableCell>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleCheckin}
+          disabled={quantity > checkedOut}
+        >
+          Check In
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCheckout}
+          disabled={quantity > available}
+        >
+          Check Out
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
+
 export default function ProjectCard({ project }) {
-  // const [checkoutRequests, setCheckoutRequests] = useState({});
-  // const [checkinRequests, setCheckinRequests] = useState({});
-
-  // const handleCheckoutRequest = (hardwareIndex, value) => {
-  //   setCheckoutRequests({ ...checkoutRequests, [hardwareIndex]: value });
-  // };
-
-  // const handleCheckinRequest = (hardwareIndex, value) => {
-  //   setCheckinRequests({ ...checkinRequests, [hardwareIndex]: value });
-  // };
-
-  // const handleCheckout = (hardwareIndex) => {
-  //   const hardware = project.hardware[hardwareIndex];
-  //   const numberToCheckOut = parseInt(checkoutRequests[hardwareIndex] || 0);
-  //   if (
-  //     numberToCheckOut > 0 &&
-  //     numberToCheckOut <= hardware.totalAvailable - hardware.numberCheckedOut
-  //   ) {
-  //     hardware.numberCheckedOut += numberToCheckOut;
-  //     hardware.totalAvailable -= numberToCheckOut;
-  //     setCheckoutRequests({ ...checkoutRequests, [hardwareIndex]: "" });
-  //   }
-  // };
-
-  // const handleCheckin = (hardwareIndex) => {
-  //   const hardware = project.hardware[hardwareIndex];
-  //   const numberToCheckIn = parseInt(checkinRequests[hardwareIndex] || 0);
-  //   if (numberToCheckIn > 0 && numberToCheckIn <= hardware.numberCheckedOut) {
-  //     hardware.numberCheckedOut -= numberToCheckIn;
-  //     hardware.totalAvailable += numberToCheckIn;
-  //     setCheckinRequests({ ...checkinRequests, [hardwareIndex]: "" });
-  //   }
-  // };
-
   console.log(project);
 
   return (
     <Card style={{ marginBottom: "20px" }}>
-      <CardContent>
-        <Typography variant="h5">{project.project_name}</Typography>
-        <Typography variant="body2">{project.description}</Typography>
-        <TableContainer component={Paper}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Resource</TableCell>
-                <TableCell>Available</TableCell>
-                <TableCell>Checked Out</TableCell>
-                <TableCell>Check in</TableCell>
-                <TableCell>Check out</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {project.hardware.map((hardware, hardwareIndex) => (
-                <TableRow key={hardwareIndex}>
-                  <TableCell>{hardware.item}</TableCell>
-                  <TableCell>{hardware.available}</TableCell>
-                  <TableCell>{hardware.checked_out}</TableCell>
-                  <TableCell>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      // onClick={() => handleCheckin(hardwareIndex)}
-                    >
-                      Check In
-                    </Button>
-                  </TableCell>
-                  <TableCell>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      // onClick={() => handleCheckout(hardwareIndex)}
-                    >
-                      Check Out
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </CardContent>
+      <Typography variant="h5">{project.project_name}</Typography>
+      <Typography variant="body2">{project.description}</Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Resource</TableCell>
+              <TableCell>Available</TableCell>
+              <TableCell>Checked Out</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell>Check in</TableCell>
+              <TableCell>Check out</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {project.hardware.map((hardware, hardwareIndex) => (
+              <HardwareRow key={hardwareIndex} hardware={hardware} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Card>
   );
 }
